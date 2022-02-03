b77bba014132d1a46cf09359685f9307439a95ff
unknown <alpasa50@gmail.com>
2022-02-03 17:14:38 -0400

    [NEW] Adding limelight code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,12 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;  
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -25,19 +31,19 @@ import edu.wpi.first.wpilibj.motorcontrol.Talon;
  * directory.
  */
 public class Robot extends TimedRobot {
-  private final WPI_TalonSRX m_frontRight = new WPI_TalonSRX(1);
-  private final WPI_TalonSRX m_rearRight = new WPI_TalonSRX(2); //Hay que confirmar el ID
+  private final WPI_TalonSRX m_frontRight = new WPI_TalonSRX(1); //Declarar el motor derecho en frente
+  private final WPI_TalonSRX m_rearRight = new WPI_TalonSRX(2); //Declarar el motor derecho detras
  
-  private final MotorControllerGroup  m_rightDrive = new MotorControllerGroup(m_frontRight, m_rearRight);
+  private final MotorControllerGroup  m_rightDrive = new MotorControllerGroup(m_frontRight, m_rearRight); //Declaramos el grupo de motores derecho
  
-  private final WPI_TalonSRX m_frontLeft = new WPI_TalonSRX(4); //Hay que confirmar el ID
-  private final WPI_TalonSRX m_rearLeft = new WPI_TalonSRX(3);
+  private final WPI_TalonSRX m_frontLeft = new WPI_TalonSRX(4); //Declarar el motor derecho en frente
+  private final WPI_TalonSRX m_rearLeft = new WPI_TalonSRX(3);  //Declarar el motor derecho detras
 
-  private final MotorControllerGroup  m_leftDrive = new MotorControllerGroup(m_frontLeft, m_rearLeft);
+  private final MotorControllerGroup  m_leftDrive = new MotorControllerGroup(m_frontLeft, m_rearLeft); //Declaramos el grupo de motores izquierdo
 
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive); //Declaramos el robotDrive utilizando los dos grupos de motores
 
-  private final Joystick my_joystick = new Joystick(0);
+  private final Joystick my_joystick = new Joystick(0); //Creamos el joystick
   //private final Timer m_timer = new Timer();
 
   @Override
@@ -45,7 +51,9 @@ public class Robot extends TimedRobot {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightDrive.setInverted(true);
+    m_rightDrive.setInverted(true); //Invertimos los motores
+    //read values periodically
+    
   }
 
   /** This function is run once each time the robot enters autonomous mode. */
@@ -66,9 +74,27 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    //m_frontRight.set(ControlMode.PercentOutput, my_joystick.getRawAxis(5));
-    m_robotDrive.arcadeDrive(-my_joystick.getRawAxis(5), my_joystick.getRawAxis(4)); //Utilizando el joystick de la derecha
+    double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+    double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+    double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
 
+
+    SmartDashboard.putNumber("Limelight-tv", tv);
+    SmartDashboard.putNumber("Limelight-tx", tx);
+    SmartDashboard.putNumber("Limelight-ty", ty);
+    SmartDashboard.putNumber("Limelight-ta", ta);
+
+    //m_frontRight.set(ControlMode.PercentOutput, my_joystick.getRawAxis(5));
+    //codigo del robot funcional arcade
+    m_robotDrive.arcadeDrive(-my_joystick.getRawAxis(5), my_joystick.getRawAxis(4)); //Utilizando el joystick de la derecha
+    
+    
+    /*Conduccion Jorge
+    m_robotDrive.arcadeDrive(-my_joystick.getRawAxis(1), my_joystick.getRawAxis(4));
+    */
+
+    //codigo del robot funcional tanque
    //m_robotDrive.tankDrive(-my_joystick.getRawAxis(1), -my_joystick.getRawAxis(5));
 
 
